0965bfc5ff642850e38b890c1571bbb5a8df3091
Tianlang <tianlangstuido@aliyun.com>
2020-04-29 16:42:56 +0800

    change readme

--- src/Portal.js
+++ src/Portal.js
@@ -18,9 +18,9 @@ class Portal extends Component {
     }
     return (
       <div style={{paddingTop:20}}>
-        <a href="/win" style={buttonStyle}>Windows系统</a>
-        <a href="/tlp" style={buttonStyle}>Windows系统</a>
+        <a href="/tlp" style={buttonStyle}>大数据平台之门户</a>
         &nbsp;&nbsp;&nbsp;
+        <a href="/win" style={buttonStyle}>Windows系统</a>
         <a href="/mac" style={buttonStyle}>Mac系统</a>
       </div>
     )
